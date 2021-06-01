4332fcc43058ab57fb0e68d82d8d223428d288be
Felipe Llinares <felipellinares@uma.es>
2021-06-01 11:39:34 +0200

    Modificacion Empleado y Usuario

--- EasyJob/src/Empleado.java
+++ EasyJob/src/Empleado.java
@@ -1,2 +1,6 @@
-public class Empleado {
+public class Empleado extends Usuario{
+
+    public Empleado(String nombre, String apellidos, String correoElectronico, String DNI, String telefono, String contrasena) {
+        super(nombre, apellidos, correoElectronico, DNI, telefono, contrasena);
+    }
 }
