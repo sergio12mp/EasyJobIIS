b7edc527b5a4722786b1811e3e84072857e51118
Felipe Llinares <felipellinares@uma.es>
2021-06-01 12:06:08 +0200

    Modificacion Jefe

--- EasyJob/src/Jefe.java
+++ EasyJob/src/Jefe.java
@@ -1,2 +1,5 @@
-public class Jefe {
+public class Jefe extends Usuario{
+    public Jefe(String nombre, String apellidos, String correoElectronico, String DNI, String telefono, String contrasena) {
+        super(nombre, apellidos, correoElectronico, DNI, telefono, contrasena);
+    }
 }
