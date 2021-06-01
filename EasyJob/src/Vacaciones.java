b10660ec56a1cb9de681fbe8f0127d38d124b3c7
Rodrigo <rodrigorolcor23@uma.es>
2021-06-01 19:07:02 +0200

    Vacaciones

--- EasyJob/src/Vacaciones.java
+++ EasyJob/src/Vacaciones.java
@@ -2,13 +2,15 @@ import java.util.Date;
 
 public class Vacaciones {
 private Date dinicio;
+    private Empleado empleado;
 private Date dfinal;
+private boolean aceptada;
 
 public Vacaciones(Date ini, Date fin){
     dinicio = ini;
     dfinal  = fin;
+    aceptada = false;
 }
-
     public Date getDinicio() {
         return dinicio;
     }
@@ -17,11 +19,29 @@ public Vacaciones(Date ini, Date fin){
         this.dinicio = dinicio;
     }
 
-    public Date getDfinal() {
-        return dfinal;
-    }
+    public Date getDfinal() {  return dfinal; }
 
     public void setDfinal(Date dfinal) {
         this.dfinal = dfinal;
+    }
+
+    public void setAceptada(boolean aceptada) {
+        this.aceptada = aceptada;
+    }
+
+    public boolean isAceptada(){
+        return aceptada;
+    }
+
+
+    public String toString() {
+        String res = "Solicitud cita de ";
+        res += empleado.getNombre() + ", para el " + dinicio + "hasta el " + dfinal + "\n ESTADO: ";
+        if (!aceptada) {
+            res += "Pendiente de confirmacion";
+        } else {
+            res += "Aceptada";
+        }
+        return res;
     }
 }
