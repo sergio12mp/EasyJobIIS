22b2f7ad6ca2d34432f3af1dd6b2c7b90e968f6f
Jose Maria Tapia <jmtapiacatena@uma.es>
2021-06-01 11:48:36 +0200

    Chat creado

--- EasyJob/src/Chat.java
+++ EasyJob/src/Chat.java
@@ -1,2 +1,39 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringJoiner;
+
 public class Chat {
+
+
+    private String nombreChat;
+    private List<Mensaje> mensajes;
+
+
+    public Chat (String nombre) {
+        nombreChat = nombre;
+        mensajes = new LinkedList<>();
+    }
+
+    public  void añadirMensaje (Mensaje m) {
+        mensajes.add(m);
+    }
+
+    public void eliminarMensaje (Mensaje m) {
+        mensajes.remove(m);
+    }
+
+    public String verMensajes () {
+        System.out.println("**********************************");
+        StringJoiner sj = new StringJoiner("\n");
+        sj.add("Chat " + nombreChat);
+        for (Mensaje m : mensajes) {
+            sj.add(m.toString());
+        }
+        sj.add("*********************************************");
+        return sj.toString();
+    }
+
+
+
+
 }
--- EasyJob/src/Mensaje.java
+++ EasyJob/src/Mensaje.java
@@ -1,2 +1,20 @@
 public class Mensaje {
+
+
+        private String autor;
+        private String asunto;
+        private String contenido;
+
+        public Mensaje (String aut, String asu, String cont){
+            autor = aut;
+            asunto = asu;
+            contenido = cont;
+        }
+
+        public String toString () {
+            return ("Usuario: " + autor + ", Asunto: " + asunto + ", Contenido: " + contenido);
+        }
 }
+
+
+
