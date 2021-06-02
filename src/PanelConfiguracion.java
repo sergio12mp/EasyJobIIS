a42e484b8f99ca16313cd419d736e5a70bb3fcb5
eperomres <sermorepe@gmail.com>
2021-06-02 22:08:33 +0200

    panelesde Configuracion

--- src/PanelConfiguracion.java
+++ src/PanelConfiguracion.java
@@ -1,7 +1,18 @@
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class PanelConfiguracion extends JPanel implements VistaEasyJob {
 
+public PanelConfiguracion(){
+
+
+
+
+}
+
+public void controlador (ActionListener ctrl){
+
+}
 
 
 }
