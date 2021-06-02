67e123ac60b12936974a6075c132e93a08811008
Sergio David <sromero@gmail.com>
2021-06-02 16:53:40 +0200

    SolicitudHorario instanciada

--- src/SolicitudHorario.java
+++ src/SolicitudHorario.java
@@ -1,2 +1,14 @@
 public class SolicitudHorario {
+    private String motivo;
+    private boolean aceptado=false;
+    public SolicitudHorario(String motivo){
+        this.motivo=motivo;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "SolicitudHorario{" +
+                "motivo='" + motivo + '\'' +
+                '}';
+    }
 }
